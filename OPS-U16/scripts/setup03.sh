0da7beddd2532cf1c674f8cc040deb348baea3a7
Tung Doan <doantungbk.203@gmail.com>
2017-10-19 01:08:37 -0700

    Update Pike installation

--- OPS-U16/scripts/setup03.sh
+++ OPS-U16/scripts/setup03.sh
@@ -26,16 +26,28 @@ if [ "$1" == "controller" ]; then
           read -p "Are you sure all compute nodes are sucessfully installed?" yn
           case $yn in
             [Yy]* ) su -s /bin/sh -c "nova-manage cell_v2 discover_hosts --verbose" nova
+                    sleep 5
+                    su -s /bin/sh -c "nova-manage cell_v2 discover_hosts --verbose" nova
+                    nova-status upgrade check
                     bash $dir_path/install/install_neutron.sh $1;;
                     bash $dir_path/install/install_horizon.sh;;
-            [Nn]* ) exit;;
+            [Nn]* ) echo "Please answer yes or no.";;
             * ) echo "Please answer yes or no.";;
           esac
         done
 
 elif [ "$1" == "compute1" ] || [ "$1" == "compute2" ]; then
 	bash $dir_path/install/install_nova.sh $1
-	bash $dir_path/install/install_neutron.sh $1
+        #Stope here and wait for other nodes to be up
+        while true; do
+          read -p "Are you sure the discovery command is run in controller node?" yn
+          case $yn in
+            [Yy]* ) bash $dir_path/install/install_neutron.sh $1;;
+            [Nn]* ) echo "Please answer yes or no.";;
+            * ) echo "Please answer yes or no.";;
+          esac
+        done
+	
 
 else
 	echocolor "Error syntax"
