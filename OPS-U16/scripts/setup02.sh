56788c992d923a5c3434f903b838cd94d5b9b56a
Tung Doan <doantungbk.203@gmail.com>
2017-10-21 10:39:29 -0700

    Update conf 2

--- OPS-U16/scripts/setup02.sh
+++ OPS-U16/scripts/setup02.sh
@@ -142,7 +142,7 @@ function install_etcd {
        chown etcd:etcd /var/lib/etcd
        ETCD_VER=v3.2.7
        rm -rf /tmp/etcd && mkdir -p /tmp/etcd
-       curl -L "https://github.com/coreos/etcd/releases/download/${ETCD_VER}/etcd-${ETCD_VER}-linux-amd64.tar.gz" -o "/tmp/etcd-${ETCD_VER}-linux-amd64.tar.gz"
+       wget "https://github.com/coreos/etcd/releases/download/${ETCD_VER}/etcd-${ETCD_VER}-linux-amd64.tar.gz" -O "/tmp/etcd-${ETCD_VER}-linux-amd64.tar.gz"
        tar xzvf /tmp/etcd-${ETCD_VER}-linux-amd64.tar.gz -C /tmp/etcd --strip-components=1
        cp /tmp/etcd/etcd /usr/bin/etcd
        cp /tmp/etcd/etcdctl /usr/bin/etcdctl
